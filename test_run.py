4f836cc342b3be62830c30306f322d844d4df707
YaoshiHu <henryhustone@outlook.com>
2018-12-19 03:46:34 +0000

    add commit

--- test_run.py
+++ test_run.py
@@ -10,17 +10,17 @@ if __name__ == "__main__":
     for i in range(1, 100):
         backend.simulate_step()
         if i == 1:
-            print(backend.get_light_definition("0d"))
+#             print(backend.get_light_definition("0d"))
             print(backend.get_light_ids())
 
-        if i % 10 == 0:
-            backend.set_light_phase("0d", 2)
+#         if i % 20 == 0:
+#             backend.set_light_phase("0d", 1)
 
         vehicle_ids = backend.get_vehicle_ids()
-        cur_state = backend.get_cur_light_state("0d")
+#         cur_state = backend.get_cur_light_state("0d")
 
         avg_passed_vehicle = 0.0
-        print("Time {}: Vehicle # are: {}\nTraffic light {}".format(i, len(vehicle_ids), cur_state))
+        print("Time {}: Vehicle # are: {}\nTraffic light".format(i, len(vehicle_ids)))
         for lane in lanes:
             avg_passed_vehicle += (backend.get_vehicle_cnt(lane) - backend.get_halt_vehicle_cnt(lane)) / float(lanes_cnt)
 
